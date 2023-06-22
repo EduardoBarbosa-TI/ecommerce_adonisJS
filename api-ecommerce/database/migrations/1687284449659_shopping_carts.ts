236811042fd77320d44015036a1e79a98bf9cebf
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-22 08:50:35 -0300

    feat: added shoppingCarts migration

---
+++ api-ecommerce/database/migrations/1687284449659_shopping_carts.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'shopping_carts'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.integer('userId').unsigned().references('users.id').onDelete('CASCADE').onUpdate('CASCADE')
+      table.string('status')
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
