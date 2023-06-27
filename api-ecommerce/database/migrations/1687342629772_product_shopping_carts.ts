583cf5c4987eaab6c19a8350b8959d9796219358
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-27 17:15:21 -0300

    style: removing comments in productShoppingCart migration

--- api-ecommerce/database/migrations/1687342629772_product_shopping_carts.ts
+++ api-ecommerce/database/migrations/1687342629772_product_shopping_carts.ts
@@ -1,17 +1,13 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
-
 export default class extends BaseSchema {
   protected tableName = 'product_shopping_carts'
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.integer('productId').unsigned().references('products.id').onDelete('CASCADE').onUpdate('CASCADE')
-      table.integer('shoppingCartId').unsigned().references('shopping_carts.id').onDelete('CASCADE').onUpdate('CASCADE')
+      table.integer('product_id').unsigned().references('products.id').onDelete('CASCADE')
+      table.integer('shopping_cart_id').unsigned().references('shopping_carts.id').onDelete('CASCADE')
       table.integer('unitProducts')
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
