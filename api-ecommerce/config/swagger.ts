e1b123b22dd89bb47d2176e39132cd749f883e6e
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-26 22:55:41 -0300

    feat: added swagger ui

---
+++ api-ecommerce/config/swagger.ts
@@ -0,0 +1,13 @@
+export default {
+  path: __dirname + "../",
+  title: "Foo",
+  version: "1.0.0",
+  tagIndex: 1,
+  ignore: ["/swagger", "/docs"],
+  preferredPutPatch: "PUT",
+  common: {
+    parameters: {},
+    headers: {},
+  },
+  snakeCase: true
+};
