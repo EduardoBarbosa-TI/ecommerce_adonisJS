3eb8840700134d8b0256ab41f7027eff80e356ce
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-27 17:13:43 -0300

    fix: declaring lucid relations in shoppingCart model

--- api-ecommerce/app/Models/ShoppingCart.ts
+++ api-ecommerce/app/Models/ShoppingCart.ts
@@ -1,7 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, HasMany, ManyToMany, column, hasMany, manyToMany } from '@ioc:Adonis/Lucid/Orm'
-import Product from './Product'
+import { BaseModel,BelongsTo,ManyToMany,belongsTo,column, manyToMany} from '@ioc:Adonis/Lucid/Orm'
 import User from './User'
+import Product from './Product'
 
 export default class ShoppingCart extends BaseModel {
   @column({ isPrimary: true })
@@ -10,16 +10,24 @@ export default class ShoppingCart extends BaseModel {
   @column()
   public status: string
 
-  @column()
-  public unitProducts: number
+  @column({columnName: 'userId'})
+  public userId: number
 
-  @hasMany(() => User)
-  public user: HasMany<typeof User>
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
+
+  @manyToMany(() => Product,{
+    localKey: 'id',
+    relatedKey: 'id',
+    pivotTable: 'product_shopping_carts',
+    pivotTimestamps: true,
+    pivotColumns: ['unitProducts'],
+  })
+  public products: ManyToMany<typeof Product>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
-
 }
