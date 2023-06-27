1bb2efcefb6f3cf8652cd9a8a2fcfc70d7b6ada1
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-26 23:02:59 -0300

    feat: added product Service

---
+++ api-ecommerce/app/Service/ProductsService.ts
@@ -0,0 +1,31 @@
+import Product from "App/Models/Product";
+import ProductsRepository from "App/Repositories/ProductsRepository";
+
+export default class ProductsServices{
+  private productsRepository: ProductsRepository
+
+  constructor(){
+    this.productsRepository = new ProductsRepository()
+  }
+
+  public async index(page: number, limit: number){
+    return await this.productsRepository.getAll(page,limit)
+  }
+
+  public async store(product: Product){
+    return await this.productsRepository.store(product)
+  }
+
+  public async show(productId: number){
+    return await this.productsRepository.getById(productId)
+  }
+
+  public async update(productId: number, product: Product){
+    return await this.productsRepository.update(productId,product)
+
+  }
+
+  public async delete(productId: number){
+    return await this.productsRepository.delete(productId)
+  }
+}
