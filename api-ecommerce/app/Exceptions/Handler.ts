56e738afa8f87a5ab7d4e2affa659fdfe75cf3ad
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-28 23:15:31 -0300

    feat: added error handling

--- api-ecommerce/app/Exceptions/Handler.ts
+++ api-ecommerce/app/Exceptions/Handler.ts
@@ -12,12 +12,35 @@
 | properly.
 |
 */
-
 import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   constructor () {
     super(Logger)
+  }
+
+  public async handle(error: any, ctx: HttpContextContract){
+    console.log(error)
+    if (error.message === 'NOT_FOUND') {
+      return ctx.response.status(404).json({
+        message: 'Not Found'
+      })
+    }
+
+    if (error.message === 'UNAUTHORIZED') {
+      return ctx.response.status(401).json({
+        message: 'Unauthorized'
+      })
+    }
+
+    if (error.message === 'INTERNAL_SERVER_ERROR') {
+      return ctx.response.status(500).json({
+
+      })
+    }
+
+    return super.handle(error,ctx)
   }
 }
