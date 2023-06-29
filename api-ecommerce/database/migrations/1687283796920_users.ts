3c2645f62ae3331a37bb2e73a8c9adc6d6fbbd88
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-28 23:19:54 -0300

    fix: adding email as unique

--- api-ecommerce/database/migrations/1687283796920_users.ts
+++ api-ecommerce/database/migrations/1687283796920_users.ts
@@ -7,7 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
       table.string('name')
-      table.string('email')
+      table.string('email').unique()
       table.string('password')
       table.string('address')
       table.boolean('admin').defaultTo(false)
